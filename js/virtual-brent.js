d15b3ad241ad73ab29c85798869e277e94b8d0fa
Zoltan Hawryluk <zhawryluk@sapient.com>
2018-01-09 16:24:58 -0500

    Redoing Image CSS to ensure it is centered all the time.

--- js/virtual-brent.js
+++ js/virtual-brent.js
@@ -7,10 +7,11 @@ var vb = new function () {
     $mouth = document.getElementById('brent-mouth'),
     $button = document.getElementById('brent-button'),
     $body = document.body,
-    mouthStyle = $mouth.style,
-    recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)(),
-    possibleResponses = [];
+    mouthStyle = $mouth.style;
 
+  /*
+   * Returns a random number between min and max (inclusive).
+   */
   function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
@@ -25,15 +26,16 @@ var vb = new function () {
         }
       })
       .then(function(text) {
-        data=text.split('\n');
-        startListening();
+        // replace &apos; with ' and remove :slack-emoji-labels:
+        data=text.replace(/&apos;/g, '\'').replace(/:[a-z0-9]:/g, '').split('\n');
         getNewQuote();
       })
       .catch(function() {
+        // give a witty alert when something goes wrong.
         alert('Brent just gave up and went home. Please try again later.');
       });;
 
-    $button.addEventListener('click', getNewQuote);
+    $body.addEventListener('click', getNewQuote);
   }
 
   function say(text) {
@@ -41,12 +43,18 @@ var vb = new function () {
       return;
     }
 
+    // let's open that mouth .
     $body.classList.add('animating');
+
     // cancel anything that is being spoken right now.
     speechSynthesis.cancel();
 
     currentText = text;
 
+    /*
+     * Since I don't know the best way to pick voices yet, we are just going to 
+     * assume the first one is the best.
+     */
     var voices = window.speechSynthesis.getVoices();
     msg = window.SpeechSynthesisUtterance ? new SpeechSynthesisUtterance() : null
     msg.voice = voices[0];
@@ -54,34 +62,42 @@ var vb = new function () {
     msg.pitch = 1;
     msg.text = text;
 
-    msg.addEventListener('boundary', onWordBoundary);
+    // this is the event that fires when the end of a word is spoken
+    msg.addEventListener('boundary', onWordBoundaryEvent);
 
-    msg.onend = function(e) {
-      $body.classList.remove('animating');
-      $quote.innerHTML = currentText;
-    }
+    // this is the event that fires once the while string is said.
+    msg.addEventListener('end', onEndEvent);
 
     requestAnimationFrame( function () {
       speechSynthesis.speak(msg);
     });
-
   }
 
   function getNewQuote(n) {
     var quoteNum;
 
-    if (n) {
+    if (typeof(n) === 'number') {
       quoteNum = n;
     } else {
       quoteNum =  randInt(0, data.length - 1);   // 82 35
     }
     
-    var quote = data[quoteNum].replace(/&apos;/g, '\'').replace(/:[^\s]+:/g, '');
+    var quote = data[quoteNum];
     
     say(quote);
   }
 
+  /*
+   * A very naive way to find the number of syllables in a word.  Uses so we
+   * can make the mouth movement look more accurate.  Not always 100% accurate,
+   * but good enough for our purposes.
+   * 
+   * This function originally from
+   * https://stackoverflow.com/questions/5686483/how-to-compute-number-of-syllables-in-a-word-in-javascript
+   *
+   */
   function syllable(word) {
+    console.log(word);
     word = word.trim();
     
     if (word.match(/^[0-9]+$/)) {
@@ -98,13 +114,15 @@ var vb = new function () {
       return matches ? matches.length : 1;                    //word.scan(/[aeiouy]{1,2}/).size
   }
 
-  function onWordBoundary(e) {
+  function onWordBoundaryEvent(e) {
     //var text = e.currentTarget.text;
     $mouth.className = '';
     var textUpToBoundary = currentText.substring(0, e.charIndex);
       var rest = currentText.substring(e.charIndex);
       var nextBoundary = rest.regexIndexOf(/\s/);
       var wordSpoken = rest.substring(0, nextBoundary);
+    
+      console.log(e, rest);
 
       if (wordSpoken.trim() === '') {
         wordSpoken = currentText.substring(currentText.lastIndexOf(' '));
@@ -116,7 +134,14 @@ var vb = new function () {
       mouthStyle.animationIterationCount = syllables;
       $quote.innerHTML = (currentText.substring(0, e.charIndex + nextBoundary + 1));
     
-    requestAnimationFrame(openMouth, 5);
+    if (rest.match(/[a-z0-9]/i)) {
+      requestAnimationFrame(openMouth, 5);
+    }
+  }
+
+  function onEndEvent(e) {
+    $body.classList.remove('animating');
+    $quote.innerHTML = currentText;
   }
 
   function openMouth() {
@@ -127,71 +152,8 @@ var vb = new function () {
     );
   }
 
-  /*
-   * Let's make Brent a Good Listener.
-   */
-  function setRecognitionEvents() {
-    [
-      /* 'audiostart',
-      'audioend',
-      'end',
-      'error',
-      'nomatch',
-      'result', 
-      'soundstart',
-      'soundend',*/
-      'speechend',
-      //'start'
-     ].forEach(function(eventName) {
-       recognition.addEventListener(eventName, function(e) {
-         console.log(eventName, e);
-       });
-     });
-
-     recognition.addEventListener('end', recognitionEndEvent);
-     recognition.addEventListener('result', resultEvent);
-     recognition.addEventListener('speechend', speechEndEvent);
-  }
-
-  function recognitionEndEvent(e) {
-    startListening();
-  }
-
-  function speechEndEvent(e) {
-    var randomQuote = 1;
-  }
-
-  function resultEvent(e) {
-    var finalResult = event.results[0][0].transcript;
-    var words = finalResult.split(/\s+/g);
-
-    possibleResponses = [];
-    for (var i=0; i<words.length; i++) {
-      var word = words[i];
-      var index = nounIndex[word];
-      if (index) {
-        possibleResponses = possibleResponses.concat(index);
-      }
-    }
-    console.log(words, possibleResponses);
-  }
-
-  function startListening() {
-    console.log('listening');
-    recognition.start();
-
-		
-		/* recognition.onresult = function() {
-			document.querySelector('#demo-echo').textContent = event.results[0][0].transcript;
-		}; */
-  };
-
 
   me.init = function () {
-		recognition.lang = 'en-US';
-		recognition.interimResults = false;
-    recognition.maxAlternatives = 1;
-    setRecognitionEvents();
     getData();
   }
 }
